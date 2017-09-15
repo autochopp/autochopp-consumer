7c5ed9606e2e941c2f0aa404b4b4c7d4bf7db2ee
pwener <phwener@gmail.com>
2017-09-15 16:49:18 -0300

    Creating register page
    Signed-off-by: pwener <phwener@gmail.com>
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>
    Signed-off-by: Edinho Gomes <edinhogomes@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { UserRegisterPage } from './../user-register/user-register';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  registerPage = UserRegisterPage;
 
+  constructor(
+    public navCtrl: NavController
+  ) { }
+
+  pushPage(): void {
+    this.navCtrl.push(UserRegisterPage);
   }
-
 }
---
+++ src/pages/user-register/user-register.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-register',
+  templateUrl: 'user-register.html',
+})
+export class UserRegisterPage {
+
+}

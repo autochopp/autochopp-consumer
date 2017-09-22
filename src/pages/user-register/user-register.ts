ff7bd0915f9538342e3ae4d407579a13da124c84
Filipe Ribeiro <filiperibeirom7@gmail.com>
2017-09-22 17:20:22 -0300

    Adding validators for user register
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>
    Signed-off-by: pwener <phwener@gmail.com>

--- src/pages/user-register/user-register.ts
+++ src/pages/user-register/user-register.ts
@@ -1,24 +1,30 @@
 import { UserService } from './../../app/user/user.service';
 import { Component } from '@angular/core';
 
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 import { User } from "../../app/user/user";
 
 @Component({
   templateUrl: 'user-register.html',
 })
 export class UserRegisterPage {
-
+  private userForm: FormGroup;
   user = new User('', '');
 
-  constructor(private userService: UserService) { }
-
-  validate() {
-    if(this.user.password !== this.user.password_confirmation) {
-      console.log("Wrong password confirmation");
-    }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {
+    this.userForm = this.formBuilder.group({
+      email: [this.user.email, Validators.required],
+      password: [this.user.password, Validators.required],
+      password_confirmation: [this.user.password_confirmation, Validators.required],
+    });
   }
 
   create() {
+    this.user = this.userForm.value;
     this.userService.create(this.user)
       .then(user => console.log("User sucessful register"))
       .catch(error => console.error(error))

351b6433e4bed64d22d62f149b5939697ab8e311
Victor Fernandes <victor.hmfd@gmail.com>
2017-11-08 13:23:46 -0200

    Adjusting chopps page
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- src/pages/chopps/chopps.ts
+++ src/pages/chopps/chopps.ts
@@ -5,35 +5,65 @@ import 'rxjs/add/operator/map';
 
 
 /**
- * Generated class for the ChoppsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ChoppsPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @Component({
   selector: 'page-chopps',
   templateUrl: 'chopps.html',
 })
 export class ChoppsPage {
-
-  orders : any [];
-
+  
+  activateChopps = [];
+  waitingPaymentChopps = [];
+  tabsValidator = true;
+  buttonColorX: string = '#e4ac06';
+  buttonColorY: string = '#6d6e72';
+  
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
     public chopppsServiceProvider: ChoppsServiceProvider
   ) 
-    {
-    // this.orders = this.getData();
+  {
     this.chopppsServiceProvider.getChopps().then(res => {
-      res.map(x => x['isHidden'] = true);
-      this.orders = res;
+      res.map(chopp => {
+        if(chopp['qrcode'] !== null && chopp['qrcode_validate'] === true){
+          chopp['isHidden'] = true;
+          this.activateChopps.push(chopp);
+        } 
+        else if(chopp['qrcode'] === null){
+          this.waitingPaymentChopps.push(chopp);
+        }
+      })
     });
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChoppsPage');
   }
-
+  
+  showActivateChopps(){
+    this.tabsValidator = true;
+    if (this.buttonColorX !== '#e4ac06'){
+      this.swapButtonColors();
+    }
+  }
+  
+  showWaitingPaymentChopps(){
+    this.tabsValidator = false;
+    if (this.buttonColorY !== '#e4ac06'){
+      this.swapButtonColors();
+    }
+  }
+  
+  swapButtonColors(){
+    let temp = this.buttonColorX;
+    this.buttonColorX = this.buttonColorY;
+    this.buttonColorY = temp;
+  }
+  
 }
